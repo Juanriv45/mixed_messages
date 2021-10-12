1cf4d127e6ddd5617102bc4cb687c0ee6099c20d
Juan Rivera <juanriv45@gmail.com>
2021-10-12 18:06:11 -0500

    Created message creator

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ const randomNumber = (array) => {
 const playerArray = ["Kobe", "Shaq", "Dirk","Jordan"];
 const pointsArray = [45,72,55,84,87,12];
 const teamArray = ["Lakers","Suns","Heat","Raptors", "Mavs"];
+
+const messageCreator = (player,points,team) =>{
+    return  `Amazing! ${player} finished the game against the ${team} with ${points} points!`
+}
