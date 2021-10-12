fae3b2795feaa1518084a365f2cf802202dabce3
Juan Rivera <juanriv45@gmail.com>
2021-10-12 17:52:40 -0500

    Added Player Data

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const randomNumber = () => {
     return Math.floor(Math.random()*5)
 };
+
+const playerArray = ["Kobe", "Shaq", "Dirk","Jordan"];
+const pointsArray = [45,72,55,84,87,12];
+const teamArray = ["Lakers","Suns","Heat","Raptors", "Mavs"];
