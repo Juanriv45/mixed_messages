cf1fa087170046ceab8072c92e36014cabe8b9e3
Juan Rivera <juanriv45@gmail.com>
2021-10-12 20:07:51 -0500

    Added Message Creator

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-const randomNumber = (array) => {
-    return Math.floor(Math.random()*array.length)
-};
-
 const playerArray = ["Kobe", "Shaq", "Dirk","Jordan"];
 const pointsArray = [45,72,55,84,87,12];
 const teamArray = ["Lakers","Suns","Heat","Raptors", "Mavs"];
 
-const messageCreator = (player,points,team) =>{
+
+const randomNumber = (array) => {
+    return Math.floor(Math.random()*array.length)
+};
+
+const message = (player,points,team) =>{
     return  `Amazing! ${player} finished the game against the ${team} with ${points} points!`
+}
+
+function messageCreator() {
+    let player = playerArray[randomNumber(playerArray)];
+    let points = pointsArray[randomNumber(pointsArray)];
+    let team = teamArray[randomNumber(teamArray)];
+
+    return message(player, points, team);
 }
